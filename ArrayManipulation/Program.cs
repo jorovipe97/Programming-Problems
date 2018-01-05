b5861170cf8ce8e7b499ff69052a1b7760aad925
Jose Romualdo Villalobos Perez <jorovipe@gmail.com>
2018-01-04 19:14:43 -0500

    First Array Manipulation solution
    This solution takes too much time for return the answer in extreme cases

--- ArrayManipulation/Program.cs
+++ ArrayManipulation/Program.cs
@@ -41,6 +41,20 @@ After second update list will be 100 200 100 100 100.
 After third update list will be 100 200 200 200 100.
 So the required answer will be 200.
 
+    About Constrains:
+    3 <= n <= 10 000 000
+    1 <= m <= 200 000
+    1 <= a <= b <= n
+    0 <= k <= 1 000 000 000
+
+    Then in the most extreme case let's suppose that
+    m = 200 000 (number of operation is)
+    a = b = 1 for all the m operations
+    k = 1 000 000 000
+
+    Then k * m = 200 000 * 1 000 000 000 = 200 000 000 000 000
+    The main array need to be of a type whose range support the former number.
+    The long is sufficient: ((2**64)/2) - 1 > k*m
  */
 namespace ArrayManipulation
 {
@@ -55,6 +69,7 @@ namespace ArrayManipulation
 
         static void Main(string[] args)
         {
+            //ReadingFile();
             Sol();
             /*newInputData = new StringReader(ReadingFile().ToString());
 
@@ -67,31 +82,40 @@ namespace ArrayManipulation
             Console.WriteLine(Console.ReadLine());
             Console.WriteLine(Console.ReadLine()); 
 
-            Console.SetIn(originalReader);
-            Console.ReadKey();*/
-
+            Console.SetIn(originalReader);*/
+            Console.ReadKey();
         }
 
         public static void Sol()
         {
+            // For test vecvtor 1
             string[] n_and_m = Console.ReadLine().Split(' ');
-            // List Lenght
-            int n = Convert.ToInt32(n_and_m[0]);
+            // Array Lenght
+            int n = Convert.ToInt32(n_and_m[0]); // 10 000 000
             // Number of operations
-            int m = Convert.ToInt32(n_and_m[1]);
+            int m = Convert.ToInt32(n_and_m[1]); // 100 000
 
             // The main array
-            int[] mainArray = new int[n];
-
+            long[] mainArray = new long[n];
+            // int size in bits: 32 bits (4 bytes)
+            /*
+             * If for example: An int array has 4000 elements
+             * 4 000int*32bits = 128 000 bits
+             * 128 000 bits / 8 bits = 16 000 bytes
+             * 16 000 bytes / 1024 bytes = 15.625 kilobyte
+             * 15.625 kilobyte / 1024 kilobyte = 0.015258 megabyte
+             */
 
             // Operations info is saved into a jagged array
-            int[][] operationList = new int[m][];
+            //int[][] operationList = new int[m][];
             string[] opInfo;
-            int maxValue = 0;
+            long maxValue = 0;
 
             // Saving the operation info into an array
             for (int i = 0; i < m; i++)
             {
+                // Iterates over 100 000 operation
+
                 // For each operation
                 // opInfo[0] = a
                 // opInfo[1] = b
@@ -102,11 +126,13 @@ namespace ArrayManipulation
                 int k = Convert.ToInt32(opInfo[2]);
 
                 // Save all the operations to perform
-                operationList[i] = new int[3] { a, b, k };
+                //operationList[i] = new int[3] { a, b, k };
 
-                // Iterates over each element in the main array
+                // Iterates over each element beetween a-1 and b-1 in the main array
                 for (int j = 0; j < mainArray.Length; j++)
                 {
+                    // In each one of the 100 000 operation iterates over the 10 000 000 of items in the mainArray long array
+                    // 100 000 * 10 000 000 = 1 000 000 000 000 iterations
                     // If j >= a-1 and j <= b-1
                     if (j >= (a - 1) && j <= (b - 1))
                     {
@@ -117,26 +143,6 @@ namespace ArrayManipulation
                     }
                 }
             }
-
-            // Reading the operation info from the previously saved array
-            //Console.WriteLine(operationList.Length);
-            // Execute each individual operation
-            // We can instead execute each operation as soon as user has inputed to the app, for avoid two loops iterations
-            /*for (int i = 0; i < operationList.Length; i++)
-            {
-                // Iterates over each element in the main array
-                for (int j = 0; j < mainArray.Length; j++)
-                {
-                    // If j >= a-1 and j <= b-1
-                    if (j >= (operationList[i][0]-1) && j <= (operationList[i][1]-1))
-                    {
-                        // mainArray[j] += k
-                        mainArray[j] += operationList[i][2];
-                        // This this values is grater than the latest maximum value found in array
-                        maxValue = (mainArray[j] > maxValue) ? mainArray[j] : maxValue;
-                    }
-                }
-            }*/
 
             Console.WriteLine(maxValue);
             //Console.ReadKey();
@@ -159,7 +165,7 @@ namespace ArrayManipulation
             StringBuilder lines = new StringBuilder();
             // The ./../../ is for search the test vectors in root project directory instead of the bin/debug directory which be ignored by .gitignore
             // About using keyword: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-statement
-            using (FileStream vectorTest = File.Open("./../../TestVector1.txt", FileMode.Open, FileAccess.Read))
+            using (FileStream vectorTest = File.Open("TestVector1.txt", FileMode.Open, FileAccess.Read))
             {
                 // The file can be readed
                 if (vectorTest.CanRead)
@@ -168,16 +174,16 @@ namespace ArrayManipulation
                     {
                         // Read all the lines
                         string line = rs.ReadLine();
+                        int index = 0;
                         while (line != null) // Line is null if the end of the input stram is reached
                         {
-
-                            // Print the actual line
-                            //Console.WriteLine(line);
-                            lines.AppendLine(line);
+                            if (index > 0) // The first line of the document have the expected output
+                                lines.AppendLine(line);
 
                             line = rs.ReadLine(); // Read the next line
+                            index++;
                         }
-                        
+
                     }
                     /*data = new byte[vectorTest.Length];
                     // The file can be readed now
@@ -200,4 +206,5 @@ namespace ArrayManipulation
             Console.WriteLine(names[0]);
         }
     }
+    
 }
